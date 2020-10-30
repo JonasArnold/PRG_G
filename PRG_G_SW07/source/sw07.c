c5e7973b0461a9b75516a068eadbd5d65ac4b5f3
Jonas Arnold <jonas.arnold@live.de>
2020-10-30 10:44:43 +0100

    solution for sw7 (light sensor)

---
+++ PRG_G_SW07/source/sw07.c
@@ -0,0 +1,125 @@
+/**
+ * @file    sw07.c
+ * @brief   Solution to homework SW07.
+ */
+#include "board.h"
+#include <limits.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+// Settings
+static const uint16_t PollingRateMs = 50; // after x ms a new measurement value is saved
+static const uint8_t DelayPerCycleMs = 10;  // how many MS to wait while measuring
+// Graphic settings
+static const uint16_t MaxChars = 100;
+
+typedef uint32_t data_t[];
+
+int main(void) {
+
+	init();
+
+	while (true) { /* run forever */
+
+		uint16_t pastMs = PollingRateMs;  // initialize with max value
+		uint32_t countMeasurement  = 0;   // number of measurements made
+		data_t *pointer = NULL;           // pointer to measurement data in heap
+
+		while (!getSW1())   /* wait until SW1 is activated */
+			;
+
+		printf("start measurement\n");
+
+		while (getSW1()) { /* as long as SW is activated */
+
+			/* measure if the measuring time is reached */
+			if (pastMs >= PollingRateMs) {
+				pastMs = 0;  // reset counter
+
+				/* measure brightness */
+				ADC1_Start();  // start conversion
+				while (!ADC1_GetStatus())	// wait until conversion is done
+					;
+				uint32_t value = ADC1_GetValue();  // get ADC value
+
+				/* reallocate memory (or allocate if the pointer is NULL)*/
+				pointer = (data_t*) realloc(pointer, (sizeof(uint32_t)*(countMeasurement+1)));
+				if(pointer == NULL){  /* check if there is storage left */
+					printf("Storage full! Value was discarded.\n");
+					break;  // exit loop
+				}
+
+				(*pointer)[countMeasurement++] = value;
+			}
+			else
+			{
+				pastMs += DelayPerCycleMs;  // add to MS counter
+			}
+			delay_ms(DelayPerCycleMs);
+
+		}   // end measurement
+
+		printf("measurement ended\n");
+
+		/* Calculate statistics */
+		uint32_t max = 0, min = UINT32_MAX, avg = 0;
+		uint64_t sum = 0;
+
+		for(size_t i=0; i<countMeasurement; i++){  // calculate maximum
+			if((*pointer)[i] > max){
+				max = (*pointer)[i];
+			}
+		}
+		for(size_t i=0; i<countMeasurement; i++){  // calculate minimum
+			if((*pointer)[i] < min){
+				min = (*pointer)[i];
+			}
+		}
+		for(size_t i=0; i<countMeasurement; i++){  // calculate average
+			sum += (*pointer)[i];
+		}
+		avg = sum/countMeasurement;
+
+		/* show result */
+		printf("numerical results:\n");
+		printf("minimum: %i, average: %i, maximum: %i\n", min, avg, max);
+		printf("Num of measurements: %i, Sum: %i\n", countMeasurement, sum);
+
+		/* calculating graphical result */
+		uint16_t maxRows = 200;
+		uint32_t valuesPerBar = 1; // default 1 value
+		if(countMeasurement > maxRows){ // more than 200 measurements
+			valuesPerBar = countMeasurement / maxRows;
+		}
+		else{  // less than 200 measurements
+			maxRows = countMeasurement;
+		}
+
+		uint32_t rows[maxRows];
+		uint32_t largestRow;
+		for(uint16_t row = 0; row < maxRows; row++){  // iterate through row
+			uint32_t sumBar = 0;
+			for(uint16_t valueCount = 0; valueCount < valuesPerBar; valueCount++){ // calculate sum of this bar
+				sumBar += (*pointer)[(row*valuesPerBar)+valueCount];
+			}
+			rows[row] = sumBar/valuesPerBar;  // calculate average of the sum
+			if(rows[row] > largestRow){   // remember maximum row
+				largestRow = rows[row];
+			}
+		}
+
+		/* show result */
+		printf("graphical result:\n");
+		for(uint16_t row = 0; row < maxRows; row++){
+			// scale this value to the largest row
+			printf("\n%*c|", (MaxChars * rows[row]) / largestRow, ' ');
+		}
+
+		// free memory
+		free(pointer);
+	}
+
+	return 0;
+}
